84e7de311a044fabb7fd1eaef4c503fb106d187d
LeHa2505 <leha25052001@gmail.com>
2024-04-05 08:26:03 +0700

    video

---
+++ src/app/pages/user/watch-video/watch-video.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-watch-video',
+  templateUrl: './watch-video.component.html',
+  styleUrls: ['./watch-video.component.less']
+})
+export class WatchVideoComponent {
+
+}
