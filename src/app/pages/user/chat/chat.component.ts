b598bf5b1c22ffe06909e5244f3714679b1830a3
LeHa2505 <leha25052001@gmail.com>
2024-05-14 02:17:20 +0700

    15/4

--- src/app/pages/user/chat/chat.component.ts
+++ src/app/pages/user/chat/chat.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { RxStompService } from 'src/app/service/chat-service/rx-stomp.service';
+
+export interface Message {
+  body: string; // Nội dung của tin nhắn
+  sender?: string; // Người gửi (tùy chọn)
+  timestamp?: string; // Thời gian gửi (tùy chọn)
+}
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.less']
 })
-export class ChatComponent {
+
+export class ChatComponent implements OnInit, OnDestroy {
   friends = [
     {
       name: 'Bạn 1',
@@ -81,4 +89,27 @@ export class ChatComponent {
     }
   ]
   searchFriend: any;
+
+  receivedMessages: string[] = [];
+  // @ts-ignore, to suppress warning related to being undefined
+  private topicSubscription: Subscription;
+
+  constructor(private rxStompService: RxStompService) {}
+
+  ngOnInit() {
+    this.topicSubscription = this.rxStompService
+      .watch('/topic/public')
+      .subscribe((message: Message) => {
+        this.receivedMessages.push(message.body);
+      });
+  }
+
+  ngOnDestroy() {
+    this.topicSubscription.unsubscribe();
+  }
+
+  onSendMessage() {
+    const message = `Message generated at ${new Date()}`;
+    this.rxStompService.publish({ destination: '/topic/public', body: message });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,19 +4,12 @@
 
 export const environment = {
   BASE_API_URI: {
-    BASE_SALE_CONTRACT_API: 'https://b4lc-testv2.onrender.com/salescontracts',
-    BASE_LETTER_OF_CREDIT_API: 'https://b4lc-testv2.onrender.com/letterofcredits',
-    BASE_INVOICE_API: 'https://b4lc-testv2.onrender.com/invoices',
-    BASE_SERVICE_API: 'https://b4lc-testv2.onrender.com',
-    BASE_SERVICE_TEST_API: 'https://b4lc-testv2.onrender.com',
-  },
-  // BASE_API_URI: {
-  //   BASE_SALE_CONTRACT_API: 'http://localhost:3000/salescontracts',
-  //   BASE_LETTER_OF_CREDIT_API: 'http://localhost:3000/letterofcredits',
-  //   BASE_INVOICE_API: 'http://localhost:3000/invoices',
-  //   BASE_SERVICE_API: 'http://localhost:3000',
-  //   BASE_SERVICE_TEST_API: 'http://localhost:3000',
-  // }, 
+    BASE_SALE_CONTRACT_API: 'http://localhost:8899/salescontracts',
+    BASE_LETTER_OF_CREDIT_API: 'http://localhost:8899/letterofcredits',
+    BASE_INVOICE_API: 'http://localhost:8899/invoices',
+    BASE_SERVICE_API: 'http://localhost:8899',
+    BASE_SERVICE_TEST_API: 'http://localhost:8899',
+  }, 
   production: true,
 };
 
